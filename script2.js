bc9f1b91ec214e7521b1a7646e082764a34e8527
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-09 13:43:05 +0200

    combined challenge 1 & 2 files into one folder
    Completed challenge 2

---
+++ script2.js
@@ -0,0 +1,17 @@
+const rent = 400;
+const tax = '8%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00; 
+const minuteOfDay = 00;
+
+// Only change below this line
+
+if (hourOfDay !== undefined && minuteOfDay !== undefined && hourOfDay === 0 && minuteOfDay === 0) {
+	const taxAsDecimal = parseFloat(tax) / 100;
+	const startingAfterTax = salary * (1 - taxAsDecimal);
+	const balance = startingAfterTax - transport - food - rent;
+
+	console.log("R " + balance.toFixed(2));
+}
