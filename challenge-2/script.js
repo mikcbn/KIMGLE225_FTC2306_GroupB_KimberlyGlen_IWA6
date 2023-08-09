45bd9c7d3ba92631d9618a865587deba1e2a66eb
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-09 13:17:28 +0200

    Created all relevant files for challenge 1 & 2
    Added simple HTML and corrected the code in JavaScript for challenge 1

---
+++ challenge-2/script.js
@@ -0,0 +1,10 @@
+const primaryPhone = 'O748105141';
+const secondaryPhone = '0219131568';
+
+// Only change below this line
+
+const primaryValid = /^[0-9]+$/.test(primaryPhone);
+const secondaryValid = /^[0-9]+$/.test(secondaryPhone);
+
+console.log('Primary phone is valid numerical string:', primaryValid);
+console.log('Secondary phone is valid numerical string:', secondaryValid);
